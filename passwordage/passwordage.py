4f920f0a26ae0b5d5b75e72c6c2546d43ca71d27
Benjamin Eskola <ben@eskola.uk>
2020-08-29 10:44:30 +0100

    Initial commit

---
+++ passwordage/passwordage.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import datetime
+import json
+import os
+import subprocess
+import sys
+
+now = datetime.datetime.now()
+ts = int(now.strftime("%s"))
+
+
+def load_data(uuid=None):
+    if uuid:
+        command = ["op", "get", "item", uuid]
+    else:
+        command = ["op", "list", "items"]
+
+    return json.loads(subprocess.run(command, stdout=subprocess.PIPE).stdout)
+
+
+def process_item(item: dict):
+    if item["templateUuid"] != "001":
+        return
+
+    password_time = datetime.datetime.strptime(item["createdAt"], "%Y-%m-%dT%H:%M:%SZ")
+
+    if now - password_time > datetime.timedelta(days=365):
+        # don't bother checking password history if they're less than a year old
+        full_item = load_data(item["uuid"])
+
+        if "passwordHistory" in full_item["details"]:
+            # item has more than one password
+            newest_password = max(
+                [p["time"] for p in full_item["details"]["passwordHistory"]]
+            )
+            password_time = datetime.datetime.fromtimestamp(newest_password)
+
+    if "ainfo" in item["overview"]:
+        if "tags" not in item["overview"]:
+            item["overview"]["tags"] = []
+        return {
+            "site": item["overview"]["title"],
+            "date": password_time.timestamp(),
+            "userid": item["overview"]["ainfo"],
+            "tags": item["overview"]["tags"],
+        }
+    else:
+        print(f"No ainfo for {full_item['overview']['title']}", file=sys.stderr)
+
+
+if os.path.isfile("passwords.json"):
+    results = json.load(open("passwords.json"))
+else:
+    data = load_data()
+
+    results = [process_item(i) for i in data]
+    results = [i for i in results if i]
+
+    results.sort(key=lambda k: k["date"])
+    json.dump(results, open("passwords.json", "w"))
+
+for login in results:
+    if "todelete" in login["tags"]:
+        continue
+
+    date = datetime.datetime.fromtimestamp(login["date"])
+
+    print(login["site"], end="")
+    site_logins = [
+        i for i in results if i["site"].casefold() == login["site"].casefold()
+    ]
+
+    if len(site_logins) > 1:
+        print(f" ({login['userid']})", end="")
+    print(f": {date} ({now - date})")
